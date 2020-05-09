168e88ce37df3d89d4e348cc9c01d0b95924371c
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2020-05-08 21:04:55 -0600

    test

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://testpam123.herokuapp.com/";
+const API_URL = "https://testpam123.herokuapp.com";
 
 class AuthService {
   login(email, password) {
